aa6901e5a997e84a659d48667442921e906b7bc4
kardux <33930514+0xkardux@users.noreply.github.com>
2019-11-29 23:25:54 +0900

    make main.py

--- binance_alt_btc_day_main.py
+++ binance_alt_btc_day_main.py
@@ -1,16 +1,41 @@
 from binance_abd_trade import BinanceAltBtcDayTrade
 from time import sleep
+import logging
+import os
+from logging import handlers
 
 
-if __name__ == '__main__':
-    with open('api/binance_ysjjah_gmail.txt', 'r') as f:
-        api_keys = f.readlines()
-    api_test = {'api_key': api_keys[0].rstrip('\n'), 'api_secret': api_keys[1]}
-    binanceABDT = BinanceAltBtcDayTrade(api_test['api_key'], api_test['api_secret'])
+def setup_logger(name):
+    log_dir = f'./log/{name}/'
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-    print('start trade')
-    binanceABDT.start_trade()
-    while True:
-        sleep(100)
-    print('stop trade')
-    binanceABDT.stop_trade()
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter)
+    logger.addHandler(stream_handler)
+
+    rotate_handler = handlers.TimedRotatingFileHandler(filename=log_dir, when='midnight', interval=1, encoding='utf-8')
+    rotate_handler.setFormatter(formatter)
+    logger.addHandler(rotate_handler)
+
+    return logger
+
+
+logger = setup_logger()
+logger.info('Set up Binance Alt Btc Day Trading...')
+
+with open('api/binance_kjss970_naver.txt', 'r') as f:
+    api_keys = f.readlines()
+api_test = {'api_key': api_keys[0].rstrip('\n'), 'api_secret': api_keys[1]}
+binanceABDT = BinanceAltBtcDayTrade(api_test['api_key'], api_test['api_secret'])
+
+logger.info('Start Binance Alt Btc Day Trading')
+binanceABDT.start_trade()
+while True:
+    sleep(10)
+
