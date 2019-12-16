225ab810c034e8920aaff9007075176aa55c8520
kardux <33930514+0xkardux@users.noreply.github.com>
2019-12-16 22:03:15 +0900

    fix bug
    fix error return bug

--- binance_adt_trade.py
+++ binance_adt_trade.py
@@ -395,11 +395,12 @@ class BinanceAltDailyTrade:
 
             limit_order_id = r3_order['limit_order_id']
             limit_order_info = self.bo.get_open_order_info(limit_order_id)
+            assert limit_order_info not in self.bo.error_list
             if limit_order_info:
                 self.logger.info(f'{trading_alt}: Cancel r3 limit order')
                 assert self.bo.cancel_order(trading_alt, limit_order_id)
             else:
-                self.logger.info(f'{trading_alt}: r2 limit order is already canceled')
+                self.logger.info(f'{trading_alt}: r3 limit order is already canceled')
 
         r2_order = trading_alt_stat['r2_order']
         if r2_order['order_list_id']:
@@ -411,6 +412,7 @@ class BinanceAltDailyTrade:
 
             limit_order_id = r2_order['limit_order_id']
             limit_order_info = self.bo.get_open_order_info(r2_order['limit_order_id'])
+            assert limit_order_info not in self.bo.error_list
             if limit_order_info:
                 self.logger.info(f'{trading_alt}: Cancel r2 limit order')
                 assert self.bo.cancel_order(trading_alt, limit_order_id)
